d2baa0a22e5acad880e283dff9f56c2c940fc312
guidesiderio <guilhermedesiderio23@gmail.com>
2025-12-02 17:19:03 -0300

    Refatora servicos.html para utilizar dados dinâmicos de serviços; ajusta views.py para incluir serviços e membros da equipe no contexto.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from .models import Service, TeamMember
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context["services"] = Service.objects.all()
+        context["team_members"] = TeamMember.objects.all()
+        return context
